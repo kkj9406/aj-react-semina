76560cda1237e7be57bbca90cb3238924bb56372
kyungjin.kim <kjkim@ajnet.co.kr>
2023-03-30 09:22:41 +0900

    사다리문제 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { useState } from "react";
+// import { solution } from "./test/Ladder";
 require("./thisAndBind/GlobalContext");
 require("./thisAndBind/ObjectMethod");
 require("./thisAndBind/ConstructorMethod");
 require("./thisAndBind/ExBind");
 
+// solution(4, [
+//   [1, 0, 1],
+//   [0, 1, 0],
+//   [0, 0, 1],
+//   [0, 0, 0],
+//   [1, 0, 0],
+// ]);
 function App() {
   return <div>asdf</div>;
 }
